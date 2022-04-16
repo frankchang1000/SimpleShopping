a6c9005bf064e66fa3be3aadc9b659a57c42039b
Thomas Chia <66394854+IdeaKing@users.noreply.github.com>
2022-04-16 12:29:17 -0400

    Update README

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 ## What is SimpleShopping?
 
+## Installation
+
+```python
+git clone https://github.com/frankchang1000/SimpleShopping.git
+cd simpleshopping
+python -m venv venv
+venv\Scripts\activate
+pip install -r requirements.txt
+```
---
+++ src/models/efficientnet_mod.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+
+from typing import Tuple
+
+
+def get_model(model_name: str = "efficientnet_b4",
+              input_shape: Tuple[int, int] = (512, 512),
+              num_classes: int = 20,
+              weights: str = "imagenet",
+              include_top: bool = False,
+              fine_tune: bool = False,
+              **kwargs) -> tf.keras.models.Model:
+    """Returns a model based on the EfficientNet-BX architecture.
+    Params:
+        model_name: str
+            The name of the model.
+        input_shape : Tuple[int, int]
+            The input shape of the model.
+        num_classes : int
+            The number of classes in the dataset.
+        weights : str
+            The weights to use for the model.
+        include_top : bool
+            Whether to include the top of the model or not.
+        pooling : str
+            The pooling layer to use.
+        kwargs : dict
+            Additional keyword arguments.
+    Returns:
+        tf.keras.models.Model
+            The model.
+    """
+    models = {
+        "efficientnet_b0": tf.keras.applications.EfficientNetB0,
+        "efficientnet_b1": tf.keras.applications.EfficientNetB1,
+        "efficientnet_b2": tf.keras.applications.EfficientNetB2,
+        "efficientnet_b3": tf.keras.applications.EfficientNetB3,
+        "efficientnet_b4": tf.keras.applications.EfficientNetB4,
+        "efficientnet_b5": tf.keras.applications.EfficientNetB5,
+        "efficientnet_b6": tf.keras.applications.EfficientNetB6,
+        "efficientnet_b7": tf.keras.applications.EfficientNetB7}
+    backbone = models[model_name](weights=weights,
+                               include_top=include_top,
+                               **kwargs)
+
+    inputs = tf.keras.layers.Input(shape=(*input_shape, 3))
+    if fine_tune == True:
+        backbone.trainable = False
+    else:
+        backbone.trainable = True
+    x = backbone(inputs)
+    x = tf.keras.layers.GlobalAveragePooling2D(
+        name="avg_pool")(x)
+    x = tf.keras.layers.Dropout(
+        0.5, name="top_dropout")(x)
+    outputs = tf.keras.layers.Dense(
+        num_classes,
+        activation="softmax",
+        name="predictions")(x)
+    return tf.keras.models.Model(
+        inputs=inputs, 
+        outputs=outputs, 
+        name=model_name + "_supergrocer")
